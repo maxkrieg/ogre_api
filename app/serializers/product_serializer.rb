a86a1e4b00008a00aa45f7dcbdc27e4082754d0e
maxkrieg <maxwell.krieg@gmail.com>
2015-07-14 14:27:55 -0400

    updates product and rental serializers, adding user/owner names and emails for access

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,9 +1,13 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :daily_cost, :category, :image_src
+  attributes :id, :title, :description, :daily_cost, :category, :image_src, :owner_name
   has_many :rentals
 
   def image_src
     "https://s3-us-west-2.amazonaws.com/ogre-images/medium/#{object.id}/#{object.image_file_name}"
+  end
+
+  def owner_name
+    object.user.first_name
   end
 
 end
--- app/serializers/rental_serializer.rb
+++ app/serializers/rental_serializer.rb
@@ -1,10 +1,18 @@
 class RentalSerializer < ActiveModel::Serializer
-  attributes :id, :product_title, :start_date, :end_date, :total_cost, :status, :message, :created_at, :renter_name
+  attributes :id, :product_title, :start_date, :end_date, :total_cost, :status, :message, :created_at, :renter_name, :owner_name, :owner_email
 
   # has_one :user, serializer: UserSerializer
 
   def renter_name
     object.user.first_name + " " + object.user.last_name
+  end
+
+  def owner_name
+    object.product.user.first_name
+  end
+
+  def owner_email
+    object.product.user.email
   end
 
 end
