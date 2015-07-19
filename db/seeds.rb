41752f74924734426fbda38d426dbeee1c24acf6
maxkrieg <maxwell.krieg@gmail.com>
2015-07-19 14:42:46 -0400

    updates to seed file, adds more start_images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-@file_one = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/image01.jpg', 'image/jpg')
-@file_two = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/image02.jpg', 'image/jpg')
-@file_three = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/image03.jpg', 'image/jpg')
+@therma_rest = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/therma-rest.jpg', 'image/jpg')
+@rei_tent = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/rei-tent.jpg', 'image/jpg')
+@hiking_poles = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/hiking-poles.jpg', 'image/jpg')
+@climbing_rope = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/climbing-rope.jpg', 'image/jpg')
+@showshoes = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/showshoes.jpg', 'image/jpg')
+@kayak = Rack::Test::UploadedFile.new(Rails.root + 'starter_images/kayak.jpg', 'image/jpg')
 
 User.destroy_all
 Product.destroy_all
@@ -22,33 +25,33 @@ alex = User.create!(first_name: 'Alex', last_name: 'Zito-Wolf', email: 'alex@gma
 puts "Created alex"
 
 ## Max Products
-max_product = max.products.create!(title: 'therma-rest pad', description: 'very lightweight and versatile sleeping pad, ideal for someone short', daily_cost: '9.99', category: 'Camping', image: @file_one)
+max_product = max.products.create!(title: 'Therma-Rest Pad', description: 'very lightweight and versatile sleeping pad, ideal for someone short', daily_cost: '9.99', category: 'Camping', image: @therma_rest)
 
-max_productTwo = max.products.create!(title: 'REI tent', description: 'great, sturdy tent.  Made for three seasons', daily_cost: '30', category: 'Camping', image: @file_two)
+max_productTwo = max.products.create!(title: 'REI tent', description: 'great, sturdy tent.  Made for three seasons', daily_cost: '30', category: 'Camping', image: @rei_tent)
 
-max_productThree = max.products.create!(title: 'Hiking Poles', description: 'made for tall people, generally awesome and lightweight', daily_cost: '10', category: 'Hiking', image: @file_three)
+max_productThree = max.products.create!(title: 'Hiking Poles', description: 'made for tall people, generally awesome and lightweight', daily_cost: '10', category: 'Hiking', image: @hiking_poles)
 puts "added products to max"
 
 ## Alex Products
-alex_product = alex.products.create!(title: 'climbing rope', description: 'very strong rope, ideal for top roping', daily_cost: '20', category: 'Climbing', image: @file_one)
+alex_product = alex.products.create!(title: 'Climbing Rope', description: 'very strong rope, ideal for top roping', daily_cost: '20', category: 'Climbing', image: @climbing_rope)
 
-alex_productTwo = alex.products.create!(title: 'Snowshoes', description: 'Size 10 snowshoes by REI, really strong and secure.', daily_cost: '15.60', category: 'Snowshoeing', image: @file_two)
+alex_productTwo = alex.products.create!(title: 'Snowshoes', description: 'Size 10 snowshoes by REI, really strong and secure.', daily_cost: '15.60', category: 'Snowshoeing', image: @snowshoes)
 
-alex_productThree = alex.products.create!(title: 'Kayaks', description: 'Two sea kayaks for rent, with spray skirts.  One is a double.', daily_cost: '19.75', category: 'Kayaking', image: @file_three)
+alex_productThree = alex.products.create!(title: 'Kayaks', description: 'Two sea kayaks for rent, with spray skirts.  One is a double.', daily_cost: '19.75', category: 'Kayaking', image: @kayak)
 puts "added products to alex"
 
 ## Max Rentals
-max_rental = alex_product.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'hey man I really love that rope!')
+max_rental = alex_product.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'That rope lookes great.  How much weight can it bear?')
 
-max_rental = alex_productTwo.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'hey man I really love that rope!')
+max_rental = alex_productTwo.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'Snow shoes look awesome, are they solid on ice?')
 
-max_rental = alex_productThree.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'hey man I really love that rope!')
+max_rental = alex_productThree.rentals.create!(user: max, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '75.67', status: 'pending', message: 'Is the other kayak exactly the same as that one?  How is the storage?')
 puts 'added rentals to max'
 
 ## Alex Rentals
-alex_rental = max_product.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'hey man I really love that rope!')
+alex_rental = max_product.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'Looks like a great pad.  Is it light, and can you sleep on rocks?')
 
-alex_rental = max_productTwo.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'hey man I really love that rope!')
+alex_rental = max_productTwo.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'How easy is the tent to set up? Does it have sleeves for the poles or clips?')
 
-alex_rental = max_productThree.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'hey man I really love that rope!')
+alex_rental = max_productThree.rentals.create!(user: alex, start_date: '02/06/2015', end_date: '02/07/2015', total_cost: '125', status: 'pending', message: 'Are those poles made of titanium?')
 puts 'added rentals to alex'
